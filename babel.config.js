b8996c62bea85804c3efd23fff0fc1fce94b9669
Jason Burke <jpburke77@gmail.com>
2025-10-25 17:21:21 -0500

    Fix: replace reanimated depreciated runOnJS w/scheduleOnRN

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,8 @@ module.exports = function (api) {
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['react-native-reanimated/plugin'],
+      ['react-native-worklets/plugin'],
+      // ['react-native-reanimated/plugin'],
     ],
   };
 };
