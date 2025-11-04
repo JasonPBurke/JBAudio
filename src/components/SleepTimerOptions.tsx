c01ce4e06d4beda1a1db1fe543c43f087cbf4844
Jason Burke <jpburke77@gmail.com>
2025-11-03 23:42:11 -0600

    Chore: build out timer options modal

--- src/components/SleepTimerOptions.tsx
+++ src/components/SleepTimerOptions.tsx
@@ -1,122 +1,123 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { colors, screenPadding } from '@/constants/tokens';
-import { Slider } from 'react-native-awesome-slider';
-import { useSharedValue } from 'react-native-reanimated';
+// import { Slider } from 'react-native-awesome-slider';
+// import { useSharedValue } from 'react-native-reanimated';
 import { useState } from 'react';
-// import FontAwesome from '@expo/vector-icons/FontAwesome';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import {
-  Settings,
-  CirclePlus,
-  CircleMinus,
-  CircleX,
-} from 'lucide-react-native';
+  TimerPickerModal,
+  // TimerPickerModalProps,
+} from 'react-native-timer-picker';
+import { Settings, CirclePlus, CircleMinus } from 'lucide-react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const SleepTimerOptions = () => {
-  const progress = useSharedValue(15);
-  const min = useSharedValue(0);
-  const max = useSharedValue(240);
+  // const progress = useSharedValue(15);
+  // const min = useSharedValue(0);
+  // const max = useSharedValue(240);
   const { bottom } = useSafeAreaInsets();
   const [showSlider, setShowSlider] = useState(false);
+  const [customTimer, setCustomTimer] = useState({ hours: 0, minutes: 0 });
 
   return (
     <View style={[styles.container, { marginBottom: bottom }]}>
-      {!showSlider ? (
-        <>
-          <View style={styles.header}>
-            <Text style={styles.title}>Sleep Timer Options</Text>
-            <TouchableOpacity>
-              <Settings
-                size={24}
-                color={colors.primary}
-                strokeWidth={1}
-                absoluteStrokeWidth
-              />
-            </TouchableOpacity>
-          </View>
+      <View style={styles.header}>
+        <Text style={styles.title}>Sleep Timer Options</Text>
+        <TouchableOpacity>
+          <Settings
+            size={24}
+            color={colors.primary}
+            strokeWidth={1}
+            absoluteStrokeWidth
+          />
+        </TouchableOpacity>
+      </View>
 
-          <View style={styles.buttonContainer}>
-            <View style={styles.buttonRow}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>15</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>30</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>45</Text>
-              </TouchableOpacity>
-            </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>15 mins</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>30 mins</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>45 mins</Text>
+          </TouchableOpacity>
+        </View>
 
-            <View style={styles.buttonRow}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>60</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>90</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>120</Text>
-              </TouchableOpacity>
-            </View>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>1 hr</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>1.5 hrs</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>2 hrs</Text>
+          </TouchableOpacity>
+        </View>
 
-            <View style={styles.buttonRow}>
-              <TouchableOpacity
-                style={[styles.button, styles.chapterEndButton]}
-              >
-                <CircleMinus
-                  size={24}
-                  color={colors.text}
-                  strokeWidth={1.5}
-                  absoluteStrokeWidth
-                />
-                <Text style={styles.buttonText}>Chapter End</Text>
-                <CirclePlus
-                  size={24}
-                  color={colors.text}
-                  strokeWidth={1.5}
-                  absoluteStrokeWidth
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.button, styles.customButton]}
-                onPress={() => setShowSlider(true)}
-              >
-                <Text style={styles.buttonText}>Custom</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </>
-      ) : (
-        <View style={styles.sliderContainer}>
+        <View style={styles.buttonRow}>
           <TouchableOpacity
-            style={styles.closeButton}
-            onPress={() => setShowSlider(false)}
+            style={[styles.button, styles.chapterEndButton]}
           >
-            <CircleX
-              size={34}
-              color={colors.icon}
+            <CircleMinus
+              size={24}
+              color={colors.textMuted}
+              strokeWidth={1.5}
+              absoluteStrokeWidth
+            />
+            <Text style={styles.buttonText}>Chapter End</Text>
+            <CirclePlus
+              size={24}
+              color={colors.textMuted}
               strokeWidth={1.5}
               absoluteStrokeWidth
             />
           </TouchableOpacity>
-          <Slider
-            progress={progress}
-            minimumValue={min}
-            maximumValue={max}
-            theme={{
-              minimumTrackTintColor: '#3B3B3B',
-              maximumTrackTintColor: '#3B3B3B',
-              // bubbleBackgroundColor: '#666',
-            }}
-            containerStyle={{ borderRadius: 2 }}
-            steps={14} //48 steps
-            forceSnapToStep
-            markStyle={{ height: 80, width: 1 }}
-          />
+          <TouchableOpacity
+            style={styles.customButton}
+            onPress={() => setShowSlider(true)}
+          >
+            <Text style={styles.buttonText}>Custom</Text>
+            {customTimer.hours === 0 && customTimer.minutes === 0 ? null : (
+              <Text style={styles.buttonText}>
+                {customTimer.hours}:
+                {customTimer.minutes < 10
+                  ? `0${customTimer.minutes}`
+                  : customTimer.minutes}
+              </Text>
+            )}
+          </TouchableOpacity>
         </View>
-      )}
+      </View>
+
+      <TimerPickerModal
+        visible={showSlider}
+        setIsVisible={setShowSlider}
+        closeOnOverlayPress
+        hideSeconds
+        maximumHours={24} //! SET TO HOURS REMAINING IN QUEUE
+        modalTitle='Custom Sleep Timer'
+        confirmButtonText='   Set   '
+        LinearGradient={LinearGradient}
+        onCancel={() => setShowSlider(false)}
+        onConfirm={(value) => {
+          setCustomTimer(value);
+          setShowSlider(false);
+        }}
+        styles={{
+          theme: 'dark',
+          contentContainer: {},
+          button: { borderRadius: 4 },
+          cancelButton: { backgroundColor: colors.background },
+          confirmButton: {
+            backgroundColor: colors.background,
+            borderColor: colors.primary,
+          },
+        }}
+      />
     </View>
   );
 };
@@ -163,7 +164,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   buttonText: {
-    color: colors.text,
+    color: colors.textMuted,
     alignSelf: 'center',
     fontSize: 16,
     fontWeight: 'bold',
@@ -176,6 +177,15 @@ const styles = StyleSheet.create({
   },
   customButton: {
     flex: 1, // Span one button
+    backgroundColor: colors.background,
+    // paddingVertical: 12,
+    paddingHorizontal: 8,
+    marginHorizontal: 15,
+    borderRadius: 4,
+    borderColor: colors.textMuted,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   sliderContainer: {
     width: '100%',
