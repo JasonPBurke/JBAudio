2c782993c6e13cd5594a42848f3b727bfbeba145
Jason Burke <jpburke77@gmail.com>
2025-11-17 18:19:53 -0600

    Fix: handle missing book artwork

--- src/helpers/artwork.ts
+++ src/helpers/artwork.ts
@@ -2,26 +2,36 @@ import * as RNFS from '@dr.pogodin/react-native-fs';
 import { updateCurrentBookArtworkUri } from '@/db/settingsQueries';
 
 export const saveArtwork = async (
-  base64Data: string | null,
-  bookTitle: string
+  artworkData: string | null,
+  bookTitle?: string
 ) => {
-  if (!base64Data) {
+  if (!artworkData) {
     await updateCurrentBookArtworkUri(null);
     return null;
   }
 
-  // Strip the data URI prefix if it exists
-  const pureBase64 = base64Data.replace(/^data:image\/\w+;base64,/, '');
+  // handle base64 data
+  if (artworkData.startsWith('data:image')) {
+    const pureBase64 = artworkData.replace(/^data:image\/\w+;base64,/, '');
+    const path = `${RNFS.TemporaryDirectoryPath}/${bookTitle ?? 'current_artwork'}.jpg`;
 
-  const path = `${RNFS.TemporaryDirectoryPath}/${bookTitle}.jpg`;
-
-  try {
-    await RNFS.writeFile(path, pureBase64, 'base64');
-    const uri = `file://${path}`;
-    await updateCurrentBookArtworkUri(uri);
-    return uri;
-  } catch (error) {
-    console.error('Error saving artwork:', error);
-    return null;
+    try {
+      await RNFS.writeFile(path, pureBase64, 'base64');
+      const uri = `file://${path}`;
+      await updateCurrentBookArtworkUri(uri);
+      return uri;
+    } catch (error) {
+      console.error('Error saving artwork from base64:', error);
+      return null;
+    }
+  } else {
+    // handle no artwork and use the default
+    try {
+      await updateCurrentBookArtworkUri(artworkData);
+      return artworkData;
+    } catch (error) {
+      console.error('Error saving artwork URI:', error);
+      return null;
+    }
   }
 };
