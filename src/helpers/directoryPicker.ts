9531e0882f1e1b3ec665c26a5c4c3d6e42943486
Jason Burke <jpburke77@gmail.com>
2025-11-23 16:19:32 -0600

    Fix: prevent dup library directories/subdirectories

--- src/helpers/directoryPicker.ts
+++ src/helpers/directoryPicker.ts
@@ -14,9 +14,20 @@ export async function directoryPicker() {
 
       const decodedPath = decodeURIComponent(pathAfterDelimiter);
       const currentPaths = (await getLibraryPaths()) || [];
-      if (!currentPaths.includes(decodedPath)) {
-        const newPaths = [...currentPaths, decodedPath];
+      const isSubpath = currentPaths.some((path) =>
+        decodedPath.startsWith(path + '/')
+      );
+      console.log('isSubpath', isSubpath);
+
+      if (!currentPaths.includes(decodedPath) && !isSubpath) {
+        // Filter out any existing paths that are subpaths of the new path
+        const updatedPaths = currentPaths.filter(
+          (path) => !path.startsWith(decodedPath + '/')
+        );
+
+        const newPaths = [...updatedPaths, decodedPath];
         await updateLibraryPaths(newPaths);
+        console.log('newPaths', newPaths);
       }
       await scanLibrary();
     }
