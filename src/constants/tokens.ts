f71798dc6a932adce79db3502e17c0a5860c72cc
Jason Burke <jpburke77@gmail.com>
2025-12-10 18:03:24 -0600

    Chore: add remove library path logic & minor styling adjustments

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -7,6 +7,9 @@ export const colors = {
   icon: '#E5E9F0',
   maximumTrackTintColor: 'rgba(129, 161, 193, .4)',
   minimumTrackTintColor: '#FFB606',
+  danger: '#FF5F56',
+  success: '#8BD649',
+  warning: '#FFCA8A',
 };
 
 //? Gold/black/purple: #1C1C1C, # 3B3B3B, #FFE002, #FFB606, #B28228, #492666, #830982, #
