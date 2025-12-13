3a5b2d092de1f43f95884118b2f3d21ae9190438
Jason Burke <jpburke77@gmail.com>
2025-12-13 17:43:57 -0600

    Chore: remove unused packages

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -2,18 +2,9 @@ import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { colors } from './tokens';
 
 export const StackScreenWithSearchBar: NativeStackNavigationOptions = {
-	// headerLargeTitle: true, // only on iOS
-	// headerLargeStyle: {
-	// 	backgroundColor: colors.background,
-	// },
-	// headerLargeTitleStyle: {
-	// 	color: colors.text,
-	// },
-	// headerBlurEffect: 'prominent',
-
-	headerTintColor: colors.text,
-	headerTransparent: true,
-	headerTitleStyle: {
-		fontSize: 36,
-	},
+  headerTintColor: colors.text,
+  headerTransparent: true,
+  headerTitleStyle: {
+    fontSize: 36,
+  },
 };
